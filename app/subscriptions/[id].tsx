924972719d69579102552edfdde187dc0f2214fd
hamza-code11 <hamza98.dev@gmail.com>
2026-09-06 03:43:15 +0500

    fixes

---
+++ app/subscriptions/[id].tsx
@@ -0,0 +1,14 @@
+import { useLocalSearchParams } from 'expo-router';
+import { Text, View } from 'react-native';
+
+const SubscriptionDetails = () => {
+    const {id} = useLocalSearchParams<{id: string}>();
+  return (
+    <View>
+      <Text>Subscription Details: {id}</Text>
+      {/* <Link href={"/"}>Go Back</Link> */}
+    </View>
+  )
+}
+
+export default SubscriptionDetails
